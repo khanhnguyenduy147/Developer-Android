4d572d1ff7ff1721a75791708adde532b679b49b
khanhnguyenduy147 <86216942+khanhnguyenduy147@users.noreply.github.com>
2023-12-17 15:00:35 +0700

    File Manager Edit

--- FileManager/app/src/main/java/com/example/filemanager/MainActivity.kt
+++ FileManager/app/src/main/java/com/example/filemanager/MainActivity.kt
@@ -1,27 +1,36 @@
 package com.example.filemanager
 
+import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.Environment
 import android.provider.Settings
 import android.view.ContextMenu
+import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.webkit.MimeTypeMap
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.EditText
 import android.widget.ListView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
 import java.io.File
+import java.io.IOException
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var listView: ListView
     private lateinit var emptyTextView: TextView
     private lateinit var currentDirectory: File
+    private lateinit var adapter_old: ArrayAdapter<String>
     private lateinit var adapter: ArrayAdapter<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,7 +47,7 @@ class MainActivity : AppCompatActivity() {
         emptyTextView = findViewById(R.id.emptyTextView)
 
         registerForContextMenu(listView)
-
+        openOptionsMenu()
         // Set the initial directory to external storage
         currentDirectory = Environment.getExternalStorageDirectory()
 
@@ -52,6 +61,66 @@ class MainActivity : AppCompatActivity() {
 
         loadFiles(currentDirectory)
     }
+
+    private fun createNewDirectory() {
+        val input = EditText(this)
+
+        val dialog = AlertDialog.Builder(this)
+            .setTitle("New Folder")
+            .setView(input)
+            .setPositiveButton("OK") { _, _ ->
+                val folderName = input.text.toString()
+                val newDirectory = File(currentDirectory, folderName)
+
+                if (newDirectory.mkdir()) {
+                    loadFiles(currentDirectory)
+                } else {
+                    // Handle folder creation failure
+                }
+            }
+            .setNegativeButton("Cancel", null)
+            .create()
+
+        dialog.show()
+    }
+    private fun createNewTextFile() {
+        val input = EditText(this)
+
+        val dialog = AlertDialog.Builder(this)
+            .setTitle("New Text File")
+            .setView(input)
+            .setPositiveButton("OK") { _, _ ->
+                val fileName = input.text.toString()
+                val newFile = File(currentDirectory, "$fileName.txt")
+
+                try {
+                    if (newFile.createNewFile()) {
+                        loadFiles(currentDirectory)
+                    } else {
+                        // Handle file creation failure
+                    }
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }
+            .setNegativeButton("Cancel", null)
+            .create()
+
+        dialog.show()
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.newFolderItem -> createNewDirectory()
+            R.id.newTextFileItem -> createNewTextFile()
+            // Add more menu items as needed
+        }
+        return true
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.options_menu, menu)
+        return true
+    }
+
 
     private fun loadFiles(directory: File) {
         title = directory.path
@@ -78,10 +147,36 @@ class MainActivity : AppCompatActivity() {
         listView.setOnItemClickListener { _, _, position, _ ->
             val selectedFile = File(directory, fileList[position])
             if (selectedFile.isDirectory) {
+                adapter_old = adapter
                 loadFiles(selectedFile)
             } else {
-                // Handle file click, e.g., open file
+                openFile(selectedFile)
             }
+        }
+    }
+
+    private fun openFile(file: File) {
+        val uri = FileProvider.getUriForFile(this, "${packageName}.fileprovider", file)
+
+        // Kiểm tra loại file
+        val mimeType = if (file.extension.equals("txt", ignoreCase = true)) {
+            "text/plain"
+        } else {
+            "image/*"
+        }
+
+        // Tạo Intent để mở file
+        val openIntent = Intent().apply {
+            action = Intent.ACTION_VIEW
+            setDataAndType(uri, mimeType)
+            addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+        }
+
+        try {
+            startActivity(openIntent)
+        } catch (e: ActivityNotFoundException) {
+            // Xử lý khi không có ứng dụng nào có thể xử lý file
+            Toast.makeText(this, "No app to handle this type of file", Toast.LENGTH_SHORT).show()
         }
     }
 
@@ -98,12 +193,36 @@ class MainActivity : AppCompatActivity() {
         when (item.itemId) {
             R.id.renameItem -> showRenameDialog(selectedFile)
             R.id.deleteItem -> showDeleteDialog(selectedFile)
+            R.id.copyItem -> showCopyDialog(selectedFile)
             // Add more context menu items as needed
         }
 
         return true
     }
+    private fun showCopyDialog(file: File) {
+        val input = EditText(this)
+        input.setText(file.name)
 
+        val dialog = AlertDialog.Builder(this)
+            .setTitle("Copy File/Folder")
+            .setView(input)
+            .setPositiveButton("OK") { _, _ ->
+                val newName = input.text.toString()
+                val destinationDirectory = getDestinationDirectory()
+
+                val newFile = File(destinationDirectory, newName)
+
+                if (file.copyTo(newFile, true) != null) {
+                    loadFiles(currentDirectory)
+                } else {
+                    // Handle copying failure
+                }
+            }
+            .setNegativeButton("Cancel", null)
+            .create()
+
+        dialog.show()
+    }
     private fun showRenameDialog(file: File) {
         val input = EditText(this)
         input.setText(file.name)
@@ -143,16 +262,24 @@ class MainActivity : AppCompatActivity() {
 
         dialog.show()
     }
+    private fun getDestinationDirectory(): File {
+        // You can implement logic to determine the destination directory for copying
+        // For simplicity, let's choose the current directory as the destination
+        return currentDirectory
+    }
     // Ghi đè phương thức để xử lý nút "Back"
     override fun onBackPressed() {
+        super.onBackPressed()
         // Nếu đang ở root directory, đóng ứng dụng
         if (currentDirectory.path == Environment.getExternalStorageDirectory().path) {
-            super.onBackPressed()
+            loadFiles(currentDirectory.parentFile)
         } else {
             // Nếu không ở root directory, quay lại thư mục trước đó
             loadFiles(currentDirectory.parentFile)
         }
     }
+
+
     override fun onSaveInstanceState(outState: Bundle) {
         // Lưu trạng thái khi ứng dụng bị hủy (ví dụ: khi xoay màn hình)
         outState.putString("currentDirectory", currentDirectory.path)
